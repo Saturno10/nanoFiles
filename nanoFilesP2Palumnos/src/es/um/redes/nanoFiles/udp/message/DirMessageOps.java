6dc0136f95ffa0ab93efe086d22961837cca5509
Alvaro Aledo Tornero <118385592+Saturno10@users.noreply.github.com>
2024-05-02 00:15:27 +0200

    Add files via upload

---
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/message/DirMessageOps.java
@@ -0,0 +1,19 @@
+package es.um.redes.nanoFiles.udp.message;
+
+public class DirMessageOps {
+
+	/*
+	 * TODO: Añadir aquí todas las constantes que definen los diferentes tipos de
+	 * mensajes del protocolo de comunicación con el directorio.
+	 */
+	public static final String OPERATION_INVALID = "invalid_operation";
+	public static final String OPERATION_LOGIN = "login";
+	public static final String OPERATION_LOGOUT = "logout";
+	public static final String OPERATION_LIST = "userlist";
+	public static final String OPERATION_REGISTER_SERVER = "register_server";
+	public static final String OPERATION_UNREGISTER_SERVER = "unregister_server";
+	public static final String OPERATION_DOWNLOADFROM = "downloadfrom";
+	public static final String OPERATION_SEARCH = "search";
+	public static final String OPERATION_PUBLISH = "publish";
+	public static final String OPERATION_FILELIST = "filelist";
+}
