81ea4ed37e86b7804e86ce097d2bfb4611462cf8
Alvaro Aledo Tornero <118385592+Saturno10@users.noreply.github.com>
2024-05-19 18:52:35 +0200

    PROYECTO ENTREGADO

---
+++ Redes-AAT-AVM/JARs/simular.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ejecutar java -jar directory.jar en un terminal
+gnome-terminal -- java -jar Directory.jar &
+
+
+# Ejecutar java -jar nanoFilesP2Palumnos.jar nf-shared1 en otro terminal
+gnome-terminal -- java -jar nanoFilesP2Palumnos.jar nf-shared1 &
+
+
+# Ejecutar java -jar nanoFilesP2Palumnos.jar nf-shared2 en otro terminal
+gnome-terminal -- java -jar nanoFilesP2Palumnos.jar nf-shared2 &
+
+
+# Ejecutar java -jar nanoFilesP2Palumnos.jar nf-shared13 en otro terminal
+gnome-terminal -- java -jar nanoFilesP2Palumnos.jar nf-shared3 &
+
